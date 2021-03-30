e09fcb43e3a3cea5ac37a4838fa6f67b50ef16ef
platonovAlexey <kalyawe4ka@gmail.com>
2021-03-30 18:06:47 +0300

    get current user from backend

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,6 +3,7 @@ import {setItem} from '@/helpers/persistanseStorage'
 
 const state = {
   isSubmitting: false,
+  isLoading: false,
   currentUser: null,
   validationErrors: null,
   isLoggedIn: null
@@ -14,12 +15,16 @@ export const mutationTypes = {
   registerFailure: '[auth] registerFailure',
   loginStart: '[auth] loginStart',
   loginSuccess: '[auth] loginSuccess',
-  loginFailure: '[auth] loginFailure'
+  loginFailure: '[auth] loginFailure',
+  getCurrentUserStart: '[auth] getCurrentUserStart',
+  getCurrentUserSuccess: '[auth] getCurrentUserSuccess',
+  getCurrentUserFailure: '[auth] getCurrentUserFailure'
 }
 
 export const actionTypes = {
   register: '[auth] register',
-  login: '[auth] login'
+  login: '[auth] login',
+  getCurrentUser: '[auth] getCurrentUser'
 }
 
 export const getterTypes = {
@@ -66,6 +71,19 @@ const mutations = {
   [mutationTypes.loginFailure](state, payload) {
     state.isSubmitting = false
     state.validationErrors = payload
+  },
+  [mutationTypes.getCurrentUserStart](state) {
+    state.isLoading = true
+  },
+  [mutationTypes.getCurrentUserSuccess](state, payload) {
+    state.isLoading = false
+    state.currentUser = payload
+    state.isLoggedIn = true
+  },
+  [mutationTypes.getCurrentUserFailure](state) {
+    state.isLoading = false
+    state.isLoggedIn = false
+    state.currentUser = null
   }
 }
 
@@ -100,6 +118,20 @@ const actions = {
         })
         .catch(result => {
           contex.commit(mutationTypes.loginFailure, result.response.data.errors)
+        })
+    })
+  },
+  [actionTypes.getCurrentUser](contex) {
+    return new Promise(resolve => {
+      contex.commit(mutationTypes.getCurrentUserStart)
+      authApi
+        .getCurrentUser()
+        .then(response => {
+          contex.commit(mutationTypes.getCurrentUserSuccess, response.data.user)
+          resolve(response.data.user)
+        })
+        .catch(() => {
+          contex.commit(mutationTypes.getCurrentUserFailure)
         })
     })
   }
