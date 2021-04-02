df41f02a155107becdfe68899869d4472f086576
platonovAlexey <kalyawe4ka@gmail.com>
2021-04-02 19:02:51 +0300

    add template to feed

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,8 +1,52 @@
 <template>
-  <h1>Feed {{ apiUrl }}</h1>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happened</div>
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+          >
+            <img :src="article.author.image" alt="user image" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username}
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+            ADD TO FAVORITES
+          </div>
+        </div>
+        <router-link
+          :to="{name: 'article', params: {slug: article.slug}}"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Reed more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      PAGINATION
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import {actionTypes} from '@/store/modules/feed'
 export default {
   name: 'McvFeed',
@@ -11,6 +55,13 @@ export default {
       type: String,
       required: true
     }
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
+    })
   },
   mounted() {
     this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
