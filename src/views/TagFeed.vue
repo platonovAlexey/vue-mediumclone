116cf0ba0de74bec95dcf18f81adc80c719a4a25
platonovAlexey <kalyawe4ka@gmail.com>
2021-04-03 21:09:51 +0300

    add your feed

---
+++ src/views/TagFeed.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home-page">
+    <McvBanner />
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <McvFeed :api-url="apiUrl" />
+        </div>
+        <div class="col-md-3">
+          <McvPopularTags />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import McvFeed from '@/components/Feed'
+import McvPopularTags from '@/components/PopularTags'
+import McvBanner from '@/components/Banner'
+export default {
+  name: 'McvTagFeed',
+  components: {
+    McvFeed,
+    McvPopularTags,
+    McvBanner
+  },
+  computed: {
+    apiUrl() {
+      const tagName = this.$route.params.slug
+      return `/articles?tag=${tagName}`
+    }
+  }
+}
+</script>
---
+++ src/views/YourFeed.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home-page">
+    <McvBanner />
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <McvFeed :api-url="apiUrl" />
+        </div>
+        <div class="col-md-3">
+          <McvPopularTags />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import McvFeed from '@/components/Feed'
+import McvPopularTags from '@/components/PopularTags'
+import McvBanner from '@/components/Banner'
+export default {
+  name: 'McvYourFeed',
+  components: {
+    McvFeed,
+    McvPopularTags,
+    McvBanner
+  },
+  data() {
+    return {
+      apiUrl: '/articles/feed'
+    }
+  }
+}
+</script>
