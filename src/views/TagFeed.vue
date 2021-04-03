4ece62591ffaed12bcb0ae98d917feaa98c2c576
platonovAlexey <kalyawe4ka@gmail.com>
2021-04-03 21:31:56 +0300

    add toggle feeds

--- src/views/TagFeed.vue
+++ src/views/TagFeed.vue
@@ -4,6 +4,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
+          <McvFeedToggle :tagName="tagName" />
           <McvFeed :api-url="apiUrl" />
         </div>
         <div class="col-md-3">
@@ -18,17 +19,21 @@
 import McvFeed from '@/components/Feed'
 import McvPopularTags from '@/components/PopularTags'
 import McvBanner from '@/components/Banner'
+import McvFeedToggle from '@/components/FeedToggle'
 export default {
   name: 'McvTagFeed',
   components: {
     McvFeed,
     McvPopularTags,
-    McvBanner
+    McvBanner,
+    McvFeedToggle
   },
   computed: {
+    tagName() {
+      return this.$route.params.slug
+    },
     apiUrl() {
-      const tagName = this.$route.params.slug
-      return `/articles?tag=${tagName}`
+      return `/articles?tag=${this.tagName}`
     }
   }
 }
